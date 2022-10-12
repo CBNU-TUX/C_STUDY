38f72dac0c7e77a6ed73bcbfa6b351650d69aa54
coomind <coomind@naver.com>
2022-10-12 14:24:51 +0900

    22.10.12

---
+++ Taehyo/2525.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+int main() {
+
+	int hour;
+	int min;
+	int time;
+
+	scanf("%d %d\n%d", &hour, &min, &time);
+
+	hour = hour + time / 60;
+
+	min = min + (time % 60);
+
+	if (min >= 60) {
+		hour++;
+		min = min % 60;
+	}
+
+	if (hour >= 24) {
+		hour = hour % 24;
+	}
+
+	printf("%d %d", hour, min);
+}
