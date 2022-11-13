65efa4c48bf5f91055a918d8ec43c99a8701618a
MinSuk2022 <102295791+MinSuk2022@users.noreply.github.com>
2022-11-13 23:23:29 +0900

    Add files via upload

---
+++ Minseok/22.11.10.c
@@ -0,0 +1,83 @@
+/*
+#include <stdio.h>
+
+int main() {
+	int arr[20];
+	int Ysum = 0, Msum = 0;
+	int N;
+	int sum = 0;
+
+	scanf_s("%d", &N);
+
+	int Yarr[20];
+	int Marr[20];
+
+	for (int i = 0; i < N; i++) {
+		scanf_s("%d", &arr[i]);
+	}
+
+	for (int i = 0; i < N; i++) {
+		Yarr[i] = (arr[i] / 30) + 1;
+		Marr[i] = (arr[i] / 60) + 1;
+	}
+
+	for (int i = 0; i < N; i++) {
+		Ysum += Yarr[i];
+		Msum += Marr[i];
+	}
+
+	int Y = 10 * Ysum;
+	int M = 15 * Msum;
+
+	if (Y < M) {
+		printf("Y %d", Y);
+	}
+	else if (Y > M) {
+		printf("M %d", M);
+	}
+	else if (Y == M) {
+		printf("Y M %d", Y);
+	}
+}
+*/
+
+/*
+#include <stdio.h>
+
+int main() {
+	int N;
+	double max;
+	double result;
+	double sum = 0;
+
+	scanf_s("%d", &N);
+
+	double arr[100];
+
+	for (int i = 0; i < N; i++) {
+		scanf_s("%lf", &arr[i]);
+	}
+
+	max = 0;
+
+	for (int i = 0; i < N; i++) {
+		if (max < arr[i]) {
+			max = arr[i];
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		arr[i] = arr[i] / max * 100;
+
+	}
+
+	for (int i = 0; i < N; i++) {
+		sum += arr[i];
+	}
+
+	result = sum / N;
+
+	printf("%lf", result);
+}
+*/
+
